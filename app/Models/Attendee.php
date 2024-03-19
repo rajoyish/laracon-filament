98a44f3d55d8e1999a53f213e33a31442d169170
Rajesh Budhathoki <rajoyish@gmail.com>
2024-03-19 09:23:28 +0545

    Using Filament Outside of Panels

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,10 +17,23 @@ class Attendee extends Model
         'email',
         'ticket_cost',
         'is_paid',
+        'conference_id',
     ];
 
     public function conference(): BelongsTo
     {
         return $this->belongsTo(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            Group::make()->columns(2)->schema([
+                TextInput::make('name')
+                    ->required()->maxLength(255),
+                TextInput::make('email')
+                    ->email()->required()->maxLength(255),
+            ]),
+        ];
     }
 }
---
+++ resources/views/livewire/conference-sing-up-page.blade.php
@@ -0,0 +1,5 @@
+<div class="min-h-screen grid place-items-center">
+    {{ $this->signUpAction }}
+    <x-filament-actions::modals />
+    @livewire('notifications')
+</div>
